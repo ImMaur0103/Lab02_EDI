a0def0c05c8d9c3554d5b7b0d621b85d8d47bfad
anzucel <andreazucely70@gmail.com>
2021-03-01 21:37:46 -0600

    Se añadó el método Singleton

--- Lab02/Extra/Singleton.cs
+++ Lab02/Extra/Singleton.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Arbol;
+using ListaDobleEnlace;
+using Lab02.Models;
 
 namespace Lab02.Extra
 {
@@ -9,9 +12,13 @@ namespace Lab02.Extra
     {
         private readonly static Singleton instance = new Singleton();
         //public ListaDoble<Jugador> listaDoble;
+        public Arbol<Arbol.Farmaco> Indice;
+        public ListaDoble<InfoFarmaco> ListaFarmacos; 
 
         private Singleton()
         {
+            Indice = new Arbol<Arbol.Farmaco>();
+            ListaFarmacos = new ListaDoble<InfoFarmaco>();
             //listaDoble = new ListaDoble<Jugador>();
         }
 
---
+++ Lab02/Models/InfoFarmaco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Lab02.Models
+{
+    public class InfoFarmaco
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string CasaProductora { get; set; }
+        public double Precio { get; set; }
+        public int Existencia { get; set; }
+    }
+}
