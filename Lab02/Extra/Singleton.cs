4ca25041fb75d399887dc69706b96da61bac497b
anzucel <andreazucely70@gmail.com>
2021-03-01 19:19:27 -0600

    Método Singleton

---
+++ Lab02/Extra/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab02.Extra
+{
+    public sealed class Singleton
+    {
+        private readonly static Singleton instance = new Singleton();
+        //public ListaDoble<Jugador> listaDoble;
+
+        private Singleton()
+        {
+            //listaDoble = new ListaDoble<Jugador>();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
